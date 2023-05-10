c68e39a49c797b45a2af9634b3f25199ac2c0dc7
kania08 <130148499+kania08@users.noreply.github.com>
2023-05-10 07:45:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ with st.sidebar:
 #Homepage
 if (selected == 'Homepage') :
      st.title(':blue[WELCOME TO OUR WEBS]')
+      st.write('Ekke Rose M.,'
+              ,'Kania Meilani,'
+              ,'Lulu lutfihana A.,' 
+              ,'Pandan Tsiqqa A.,'
+              ,'Stephanny Aprillilova')
         
 from PIL import Image
 image = Image.open('Image 2023-05-10 at 20.25.25.jpg')
