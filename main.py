bdafb1aafba83e06114ca27700bfc7ac6cb285e8
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-11 11:37:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ st.image(image, caption='labs')
      
         
 #Latihan soal
-Soal=st.selecbox(
+Soal=st.selectbox(
     'pilih soal'
     ('Soal 1','Soal 2','Soal 3','Soal 4','Soal 5','Soal 6','Soal 7','Soal 8','Soal 9','Soal 10'))
     
