44f360272e67c45de79b8b88eebb70a7af572436
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-11 11:37:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ st.image(image, caption='labs')
         
 #Latihan soal
 Soal=st.selectbox(
-    'pilih soal'
+    'pilih soal',
     ('Soal 1','Soal 2','Soal 3','Soal 4','Soal 5','Soal 6','Soal 7','Soal 8','Soal 9','Soal 10'))
     
 if Soal == "Soal 1":
