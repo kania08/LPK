ae8b2f552118883bac7ff90875acc5c4b6e9f960
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-11 21:10:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -232,8 +232,6 @@ if (selected == 'Pengolahan Data Titrimetri'):
         
 #Analisa data kimia organik       
 if (selected == 'Analisa Data Kimia Organik'):
-    st.subheader('Uji Ceric Nitrat')
-    st.write("uji ceric nitrat adalah cara untuk menguji larutan untuk keberadaan alkohol atau fenol. Dalam larutan, amonium nitrat oranye-kuning membentuk kompleks dengan alkohol atau fenol, menghasilkan perubahan warna. Alkohol menyebabkan perubahan warna merah, sedangkan fenol menyebabkan perubahan warna merah tua menjadi coklat tua, tergantung fenol yang digunakan.") 
     sampel=st.write_input('masukkan sampel')
     ujisampel=st.write_input('masukkan uji sampel')
     tombol=st.button('warna akhir sampel')
