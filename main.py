000b0fb73a7b09bc2090ba106dfec6520cda4ced
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-13 21:45:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if (selected == 'Latihan'):
     'pilihan soal',
     ('Soal 1','Soal 2','Soal 3','Soal 4','Soal 5','Soal 6','Soal 7','Soal 8','Soal 9','Soal 10'))
 
-If Soal == "Soal 1":
+if Soal == "Soal 1":
     st.write("titran pada standardisasi alkalimetri adalah...")
     col1, col2 = st.columns (2)
     with col1:
