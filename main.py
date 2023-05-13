eaaf01be3a086a1afe8b79d2e5604050bf9e655f
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-13 21:44:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ if (selected == 'Latihan'):
   Soal = st.selectbox(
     'pilihan soal',
     ('Soal 1','Soal 2','Soal 3','Soal 4','Soal 5','Soal 6','Soal 7','Soal 8','Soal 9','Soal 10'))
-if Soal == "Soal 1":
+
+If Soal == "Soal 1":
     st.write("titran pada standardisasi alkalimetri adalah...")
     col1, col2 = st.columns (2)
     with col1:
