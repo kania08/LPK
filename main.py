92e5d60ee095786c7de364e5c6105092fd185f4f
kania08 <130148499+kania08@users.noreply.github.com>
2023-05-16 20:02:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -294,7 +294,8 @@ if (selected == 'Pengolahan Data Analisis Titrimetri'):
    Beratekivalen = st.number_input('Masukkan berat ekivalen dalam sample')
    tombol=st.button('hitung normalitas')
    if tombol:
-        nilainormalitas=(bobot/(volume*Beratekivalen),round(4))
+        nilainormalitas=(bobot/(volume*Beratekivalen)
+                         format='%.4f')
         st.success(f'nilai normalitas adalah{nilai_normalitas}')
        
 if (selected == 'Analisa Data Kimia Organik'):
